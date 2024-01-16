972fb83a48ee7523a6ed6e9c888f46b7fa2003a0
krex <ralwnsgus4715@gmail.com>
2024-01-16 14:17:51 +0900

    update pytorch

--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 import numpy as np
 import cv2
+import json
+import matplotlib.pyplot as plt
 
+
+## For Webcam.py
 def get_most_min_distance(box_center, frame_center):
     if len(box_center) == 0:  # 객체가 감지되지 않으면 -1 반환
         return -1
@@ -37,3 +41,71 @@ def draw_skeleton(cv2_frame, keypoints, pairs):
 
     cv2_frame = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB)
     return cv2_frame
+
+
+
+
+
+
+## For preprocess.ipynb
+def load_json(json_path):
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def normalization(keypoints):
+    # 만약 2차원이면 3차원으로 만들어서 처리한후, 다시 2차원으로 만들어준다.
+    input_shape = keypoints.shape
+    if len(input_shape) == 2:
+        keypoints = keypoints.reshape(-1,17,2)
+    center_head_y = keypoints[:,0,1]
+    center_foot_y = (keypoints[:,15,1] + keypoints[:,16,1])/2
+    center_foot_00 = (keypoints[:,15,:] + keypoints[:,16,:])/2
+
+
+    # 1. 15,16번점의 중점을 (0,0)으로 만들어준다.
+    keypoints = keypoints - center_foot_00.reshape(-1,1,2)
+
+    # 2. 0번점의 Y축 값과 15,16의 Y축값의 길이를 0과 1로 만들어준다.
+    height = center_head_y - center_foot_y
+    keypoints= keypoints/ height.reshape(-1,1,1)
+
+
+    if len(input_shape) == 2:
+        keypoints = keypoints.reshape(17,2)
+    return keypoints
+
+
+def body_drawing(sample):
+    # Head 0 ~ 4
+    for i in range(5):
+        plt.scatter(sample[i, 0],sample[i, 1], s=10, color='red')
+
+    # Body
+    plt.plot((sample[5, 0],sample[6, 0]),(sample[5, 1],sample[6, 1]), color='blue')
+    plt.plot((sample[6, 0],sample[12, 0]),(sample[6, 1],sample[12, 1]), color='blue')
+    plt.plot((sample[12, 0],sample[11, 0]),(sample[12, 1],sample[11, 1]), color='blue')
+    plt.plot((sample[11, 0],sample[5, 0]),(sample[11, 1],sample[5, 1]), color='blue')
+
+    # Left Arm
+    plt.plot((sample[5, 0],sample[9, 0]),(sample[5, 1],sample[9, 1]), color='green')
+    plt.plot((sample[9, 0],sample[7, 0]),(sample[9, 1],sample[7, 1]), color='green')
+    plt.plot((sample[7, 0],sample[5, 0]),(sample[7, 1],sample[5, 1]), color='green')
+
+    # Right Arm
+    plt.plot((sample[6, 0],sample[10, 0]),(sample[6, 1],sample[10, 1]), color='green')
+    plt.plot((sample[10, 0],sample[8, 0]),(sample[10, 1],sample[8, 1]), color='green')
+    plt.plot((sample[8, 0],sample[6, 0]),(sample[8, 1],sample[6, 1]), color='green')
+
+    # Left Leg
+    plt.plot((sample[11, 0],sample[13, 0]),(sample[11, 1],sample[13, 1]), color='purple')
+    plt.plot((sample[13, 0],sample[15, 0]),(sample[13, 1],sample[15, 1]), color='purple')
+    plt.plot((sample[15, 0],sample[11, 0]),(sample[15, 1],sample[11, 1]), color='purple')
+
+    # Right Leg
+    plt.plot((sample[12, 0],sample[14, 0]),(sample[12, 1],sample[14, 1]), color='purple')
+    plt.plot((sample[14, 0],sample[16, 0]),(sample[14, 1],sample[16, 1]), color='purple')
+    plt.plot((sample[16, 0],sample[12, 0]),(sample[16, 1],sample[12, 1]), color='purple')
+
+
+    plt.grid()
